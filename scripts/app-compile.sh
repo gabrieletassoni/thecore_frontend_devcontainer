8919af37c7ed8693caf51061cef977cbc835fcb5
Gabriele Tassoni <g.tassoni@bancolini.com>
2022-11-05 04:41:44 +0000

    Inital version for test

--- scripts/app-compile.sh
+++ scripts/app-compile.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -e
 
-CURDIR=$(pwd)
+# Saving original state
+rm -rf /tmp/src.orig
+cp --verbose -rf src /tmp/src.orig
+
+# Actual build of the solution
 yarn install --cache-folder .yarn
-ionic build
+rm -rf dist
+ionic build --prod --release
 
 export IMAGE_TAG_FRONTEND=${CI_REGISTRY_IMAGE}/frontend:$CI_COMMIT_TAG
 echo "Building $IMAGE_TAG_FRONTEND"
@@ -10,17 +15,25 @@ echo "Building $IMAGE_TAG_FRONTEND"
 
 echo "Compiling custom images"
 TARGETDIR="${CI_PROJECT_DIR:-.}/customers/"
-[[ -d "$TARGETDIR" ]] && find "$TARGETDIR" -name Dockerfile | while read -r file; do
+[[ -d "$TARGETDIR" ]] && find "$TARGETDIR" -name docker.env | while read -r file; do
     echo "Compiling a custom image for: $file";
     # Looking if there is a custom script
     DIRNAME=$(dirname "$file")
-    # Must check the logic here, not sure it's ok to do this:
-    git checkout .
+    # Redo the original src
+    rm -rf src
+    cp --verbose -rf /tmp/src.orig src
+    
+    # Replace src custom files into original position
     cp --verbose -rf ${DIRNAME}/src/* src/
+
+    # Actual build of the solution
+    yarn install --cache-folder .yarn
+    rm -rf dist
+    ionic build --prod --release
 
     IMAGE_TAG_FRONTEND=${CI_REGISTRY_IMAGE}/frontend-$(basename "$DIRNAME"):$CI_COMMIT_TAG
 
     export IMAGE_TAG_FRONTEND
     echo "Building $IMAGE_TAG_FRONTEND"
-    /usr/bin/docker-build.sh "/etc/thecore/docker/Dockerfile.customer"
+    /usr/bin/docker-build.sh "/etc/thecore/docker/Dockerfile"
 done
--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -9,9 +9,7 @@ DOCKERFILE_LOCATION="$1"
 
 echo "Using $DOCKERFILE_LOCATION for build and files from $DIRNAME"
 
-docker build -f "$DOCKERFILE_LOCATION" --no-cache --pull -t "${IMAGE_TAG_FRONTEND}" \
-    --build-arg "CI_REGISTRY_IMAGE=${CI_REGISTRY_IMAGE}" \
-    --build-arg "CI_COMMIT_TAG=${CI_COMMIT_TAG}" .
+docker build -f "$DOCKERFILE_LOCATION" --no-cache --pull -t "${IMAGE_TAG_FRONTEND}"
 
 echo "Login at $CI_REGISTRY"
 docker login -u "$CI_REGISTRY_USER" -p "$CI_REGISTRY_PASSWORD" "$CI_REGISTRY"
